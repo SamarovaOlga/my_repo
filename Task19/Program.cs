731742e8ac566be5828f2c9455bbee2bf5901cf9
Olga <1234@example.com>
2022-08-18 19:09:21 +0300

    Добавила решение задачи

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -8,3 +8,15 @@
 
 23432 -> да
 */
+Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"-> да");
+    }
+    else Console.WriteLine($"-> нет");
+}
+
+ 
